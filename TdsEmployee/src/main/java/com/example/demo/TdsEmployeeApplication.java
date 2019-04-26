11f56b8f01f50d13fb7fd7b9e882f46a73dc0694
DevaCharwaka <47957031+DevaCharwaka@users.noreply.github.com>
2019-04-26 13:58:45 +0530

    Add files via upload

---
+++ TdsEmployee/src/main/java/com/example/demo/TdsEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdsEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdsEmployeeApplication.class, args);
+	}
+
+}
